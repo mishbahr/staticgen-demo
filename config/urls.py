9e517ef2ed55487b333a4db866883789a4406926
Mishbah Razzaque <mishbah@jp74.com>
2015-11-27 12:56:55 +0000

    Add robots.txt

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.utils.translation import ugettext_lazy as _
 from django.views import defaults as default_views
+from django.views.generic import TemplateView
 
 from cms.sitemaps import CMSSitemap
 
@@ -24,8 +25,9 @@ sitemaps = {
 
 urlpatterns = [
     url(r'^manage/', include(admin.site.urls)),
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
-        name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='sitemap.xml'),
+    url(r'^robots\.txt$', TemplateView.as_view(
+        template_name='robots.txt', content_type='text/plain'), name='robots.txt'),
     url(r'^404/$', default_views.page_not_found, name='page_not_found'),
     url(r'^500/$', default_views.server_error, name='server_error'),
     url(r'^', include('cms.urls')),
