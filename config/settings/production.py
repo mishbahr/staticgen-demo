acf96c2f3155cf121444df6d7b1184866c687d9c
Mishbah Razzaque <mishbah@jp74.com>
2015-11-29 13:26:58 +0000

    Remove testserver from ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -24,7 +24,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['.amazonaws.com', '.herokuapp.com', 'testserver'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['.amazonaws.com', '.herokuapp.com'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ('gunicorn', )
