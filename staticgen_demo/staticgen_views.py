2d9fce5715b2d7d5b920d2e77212f076e9ebd1be
Mishbah Razzaque <mishbah@jp74.com>
2015-11-29 01:38:07 +0000

    Add CMS Pages to staticgen registry.

--- staticgen_demo/staticgen_views.py
+++ staticgen_demo/staticgen_views.py
@@ -2,6 +2,9 @@
 
 from __future__ import unicode_literals
 
+from django.conf import settings
+from django.utils import translation
+
 from staticgen.staticgen_pool import staticgen_pool
 from staticgen.staticgen_views import StaticgenView
 
@@ -17,3 +20,27 @@ class StaicgenDemoStaticViews(StaticgenView):
         )
 
 staticgen_pool.register(StaicgenDemoStaticViews)
+
+
+class StaticgenCMSView(StaticgenView):
+
+    def items(self):
+        try:
+            from cms.models import Title
+        except ImportError:  # pragma: no cover
+            # django-cms is not installed.
+            return super(StaticgenCMSView, self).items()
+
+        items = Title.objects.public().filter(
+            page__login_required=False,
+            page__site_id=settings.SITE_ID,
+        ).order_by('page__path')
+        return items
+
+    def url(self, obj):
+        translation.activate(obj.language)
+        url = obj.page.get_absolute_url(obj.language)
+        translation.deactivate()
+        return url
+
+staticgen_pool.register(StaticgenCMSView)
