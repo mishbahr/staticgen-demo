a1300dc059bd4eeb44654b75132c3e542caa29cc
Mishbah Razzaque <mishbah@jp74.com>
2015-11-30 15:29:16 +0000

    Add print statements to debug BlogPostListView

--- staticgen_demo/blog/staticgen_views.py
+++ staticgen_demo/blog/staticgen_views.py
@@ -15,6 +15,20 @@ class BlogPostListView(StaticgenView):
     def items(self):
         return ('blog:posts_list', )
 
+    def _get_paginator(self, url):
+        response = self.client.get(url)
+        print response.status_code
+        print response.__dict__
+
+        if not response.status_code == 200:
+            pass
+        else:
+            try:
+                return response.context['paginator'], response.context['is_paginated']
+            except KeyError:
+                pass
+        return None, False
+
 
 class BlogPostDetailView(StaticgenView):
     i18n = True
@@ -25,3 +39,5 @@ class BlogPostDetailView(StaticgenView):
 
 staticgen_pool.register(BlogPostListView)
 staticgen_pool.register(BlogPostDetailView)
+
+
